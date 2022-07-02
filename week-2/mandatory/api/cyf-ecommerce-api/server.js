3f8837464aae888372c3cf0c6271a9fb607855f3
ratel4150 <rratel76@gmail.com>
2022-07-02 11:02:45 +0200

    finish goal

--- week-2/mandatory/api/cyf-ecommerce-api/server.js
+++ week-2/mandatory/api/cyf-ecommerce-api/server.js
@@ -6,13 +6,25 @@ const { Pool } = require('pg');
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
-    database: 'cyf_hotels',
-    password: '',
+    database: 'cyf_ecommerce',
+    password: 'Laura@@2009080220',
     port: 5432
 });
 
-app.get("/hotels", function(req, res) {
-    pool.query('SELECT * FROM hotels', (error, result) => {
+app.get("/customers", function(req, res) {
+    pool.query('SELECT * FROM customers', (error, result) => {
+        res.json(result.rows);
+    });
+});
+
+app.get("/suppliers", function(req, res) {
+    pool.query('SELECT * FROM suppliers', (error, result) => {
+        res.json(result.rows);
+    });
+});
+
+app.get("/products", function(req, res) {
+    pool.query('select product_name ,unit_price,supplier_name,country from products as t1 inner join suppliers as t2 on t1.supplier_id = t2.id', (error, result) => {
         res.json(result.rows);
     });
 });
